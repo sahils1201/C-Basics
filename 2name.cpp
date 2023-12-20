35440d54129fb846b6f6bf8cbb5e217213eadabe
Sahil <sahilsable737@gmail.com>
2023-12-20 14:39:14 +0530

    print_name

---
+++ 2name.cpp
@@ -0,0 +1,13 @@
+//Print name
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+
+    cout<<"Your name is "<<name;
+    return 0;
+}
