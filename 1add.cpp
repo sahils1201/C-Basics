249979bb3a153fda64fc00e15b698a43998d991b
Sahil <sahilsable737@gmail.com>
2023-12-20 14:37:50 +0530

    addn

---
+++ 1add.cpp
@@ -0,0 +1,16 @@
+//Add two nums
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter number a: ";
+    cin>>a;
+    cout<<"Enter number b: ";
+    cin>>b;
+    sum=a+b;
+    cout<<"The sum of "<<a<<" & "<<b<<" is "<<sum;
+    return 0;
+}
+
